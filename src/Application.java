41235fff0328d62e658a0f8b41b2acdda894292f
evgeny_mordyasov <michisig11i7@gmail.com>
2021-02-17 16:08:41 +0700

    change

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@ public class Application
 {
     public static void main(String[] args)
     {
-        String ACCESS_TOKEN = "qMKLwz5N-wUAAAAAAAAAAW-A44s9-6tsRvMjcmKxwgWzdH2y2tlnhWHRtR9e9F2T";
-        SoundRecorder.uploadToDropbox(ACCESS_TOKEN);
+        //String ACCESS_TOKEN = "qMKLwz5N-wUAAAAAAAAAAW-A44s9-6tsRvMjcmKxwgWzdH2y2tlnhWHRtR9e9F**";
+        //SoundRecorder.uploadToDropbox(ACCESS_TOKEN);
     }
 }
